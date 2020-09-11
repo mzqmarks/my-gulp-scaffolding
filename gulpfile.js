52194aca892af15e6514c43e532b0929d6499761
zkda <zkda@163.com>
2020-09-11 09:48:59 +0800

    0911开发服务器

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 // 实现这个项目的构建任务
 
 const { src, dest, parallel, series } = require('gulp')
+// 删除文件
+const del = require('del')
+// 开发服务器
+const browserSycn = require('browser-sync')
+const bs = browserSycn.create()
 const plugins = require('gulp-load-plugins')() // 自动加载模块
 
 const data = {
@@ -43,11 +48,15 @@ const data = {
     pkg: require('./package.json'),
     date: new Date()
 }
+// 删除文件
+const clean = () => {
+  return del(['dist'])
+}
 
 // 编译样式
 const style = () => {
     return src('src/assets/styles/*.scss', { base: 'src'})
-        .pipe(plugins.sass())
+        .pipe(plugins.sass({ outputStyle: 'expanded'}))
         .pipe(dest('dist'))
 }
 
@@ -79,7 +88,30 @@ const font = () => {
         .pipe(dest('dist'))
 }
 
+// 处理 public 文件夹
+const extra = () => {
+  return src('public/**', {base: 'public'})
+    .pipe(dest('dist'))
+}
+
+// 开启服务器
+const serve = () => {
+  bs.init({
+    files: 'dist/**',
+    server: {
+      baseDir: 'dist',
+      routes: {
+        '/node_modules': 'node_modules'
+      }
+    }
+  })
+}
+
 const compile = parallel(style, script, page, image, font)
+const build = series(clean, parallel(compile, extra))
 module.exports = {
-    compile
+    compile,
+    build,
+    clean,
+    serve
 }
